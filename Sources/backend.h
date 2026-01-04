a936f9bcc8462f58ebc7b8156a46628c5e24c980
Martin <clexan.priv@protonmail.com>
2026-01-04 17:37:01 +0100

    backend: replace windows headers with QT in header file

--- Sources/backend.h
+++ Sources/backend.h
@@ -6,11 +6,13 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include <dirent.h>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-#include <windows.h>
+
+#include <QDir>
+#include <QString>
+#include <QFileInfo>
 
 #define SR_BUFFER_MARGIN 100
 #define SR_BLOCK_SIZE    1e6
@@ -22,8 +24,6 @@ using namespace std;
 string sr_getCurrentPath();
 string sc_getLastDirectoryName(string address);
 vector<string> sr_findImgs(string dirname);
-void sr_checkProjDir();
-void sr_ask2Replace(vector<string> imgs);
 void sr_mkdir(string path);
 int  isValidImage(string name);
 
