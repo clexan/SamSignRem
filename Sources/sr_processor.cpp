676fd71523855ddb584d3864c412be8e2c05ce5d
Martin <clexan.priv@protonmail.com>
2026-01-04 18:40:33 +0100

    rewrite SrProcessor to use qfile for I/O

--- Sources/sr_processor.cpp
+++ Sources/sr_processor.cpp
@@ -8,26 +8,33 @@ SrProcessor::SrProcessor()
 void SrProcessor::processFile(string base_name)
 {
     sr_mkdir("patched");
-    string full_path = sr_getCurrentPath() + "\\lz4\\" + base_name;
-    string raw_path = sr_getCurrentPath() + "\\patched\\" + base_name;
-    img_file = fopen(full_path.c_str(), "rb");
-    raw_file = fopen(raw_path.c_str(), "wb");
-    if( img_file==NULL )
+
+    QString full_path = QString::fromStdStrings(sr_getCurrentPath()) + "/lz4/" + QString::fromStdStrings(base_name);
+    QString raw_path = QString::fromStdStrings(sr_getCurrentPath()) + "/patched/" + QString::fromStdStrings(base_name);
+   
+    img_file.setFileName(full_path);
+    raw_file.setFileName(raw_path);
+   
+    if(!img_file.open(QIODevice::ReadOnly))
     {
-        cout << "Error: cannot open file: " << full_path << endl;
-        fclose(raw_file);
+        cout << "Error: cannot open file: " << full_path.toStdString() << endl;
+        return;
+    }
+
+    if (!raw_file.open(QIODevice::WriteOnly))
+    {
+        cout << "Error: cannot create file: " << raw_path.toStdString() << endl;
+        img_file.close();
         return;
     }
 
     cout << "Processing " << base_name << endl;
 
-    string pattern = "SignerVer02";
-    // add null bytes
-    pattern.resize(pattern.length()+3, '\0');
+    QByteArray pattern = "SignerVer02";
+    pattern.append(3, '\0');
 
-    string rep;
-//    rep.resize(SR_REPLACE_SIZE, '\0');
-    rep = {0x53, 0x69, 0x67, 0x6E, 0x65, 0x72, 0x56, 0x65, 0x72,
+    const char replaceData[] = {
+           0x53, 0x69, 0x67, 0x6E, 0x65, 0x72, 0x56, 0x65, 0x72,
            0x30, 0x32, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x38,
            0x37, 0x32, 0x38, 0x39, 0x37, 0x34, 0x52, 0x00, 0x00,
            0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0x33, 0x34, 0x36,
@@ -42,161 +49,151 @@ void SrProcessor::processFile(string base_name)
            0x00, 0x00, 0x00, 0x00, 0x53, 0x52, 0x50, 0x56, 0x4C,
            0x30, 0x32, 0x41, 0x30, 0x30, 0x34, 0x00, 0x00, 0x00,
            0x00, 0x00, 0x53, 0x52, 0x50, 0x56, 0x4C, 0x30, 0x32,
-           0x41, 0x30, 0x30, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00};
+           0x41, 0x30, 0x30, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        QByteArray rep = QByteArray::fromRawData(replaceData, sizeof(replaceData));
 
     fillPositions(pattern);
     replaceBytes(rep);
 
-    fclose(raw_file);
-    fclose(img_file);
+    raw_file.close();
+    img_file.close();
 }
 
-void SrProcessor::replaceBytes(string replacement)
+void SrProcessor::replaceBytes(const QByteArray &replaceBytes)
 {
-    fseek(img_file, 0, SEEK_SET);
+    img_file.seek(0);
     curr_pos = 0;
     int pos_len = positions.size();
+
     for( int i=0 ; i<pos_len ; i++ )
     {
         rwUntilPosition(positions[i]);
         printAscii(i, replacement.length());
         printHex(i, replacement.length());
 
-        fwrite(replacement.c_str(), 1,
-               replacement.length(), raw_file);
+        raw_file.write(replacement);
         curr_pos += replacement.length();
-        _fseeki64(img_file, curr_pos, SEEK_SET);
+
+        img_file.seek(curr_pos);
     }
 
-    while( 1 )
+    while(true)
     {
-        int read_size = fread(f_buffer, 1,
-                              SR_BLOCK_SIZE, img_file);
-        fwrite(f_buffer, 1, read_size, raw_file);
+        QByteArray buffer = img_file.read(SR_BLOCK_SIZE);
+        if (buffer.isEmpty()) break;
 
-        if( read_size<SR_BLOCK_SIZE )
-        {
-            break;
-        }
+        raw_file.write(buffer);
     }
-
-    free(f_buffer);
 }
 
-void SrProcessor::fillPositions(string pattern)
+void SrProcessor::fillPositions(const QByteArray &pattern)
 {
     positions.clear();
     int counter = 0;
-    size_t read_size;
-    f_buffer = (char *)malloc(SR_BLOCK_SIZE+SR_BUFFER_MARGIN);
     curr_pos = 0;
-
-    while( 1 )
+// removed the manual malloc from f_buffer - use local qbytearray
+    while(true)
     {
-        read_size = fread(f_buffer, 1,
-                          SR_BLOCK_SIZE+SR_BUFFER_MARGIN, img_file);
-        string block(f_buffer, read_size);
-        /// FIXME: based on seek it should change
-        findPattInBlock(&block, pattern);
-        if( read_size<SR_BLOCK_SIZE )
+        QByteArray block = img_file.read(SR_BLOCK_SIZE + SR_BUFFER_MARGIN);
+
+        if (block.isEmpty()) break;
+        findPattInBlock(block, pattern);
+
+        if (block.size() < SR_BLOCK_SIZE)
         {
             break;
         }
 
         counter++;
-//        cout << "count " << counter << " "
-//             << curr_pos << "\n";
-        curr_pos = counter*SR_BLOCK_SIZE-SR_BUFFER_MARGIN;
-        _fseeki64(img_file, curr_pos, SEEK_SET);
+        curr_pos = counter * SR_BLOCK_SIZE - SR_BUFFER_MARGIN;
+
+        if (curr_pos < 0) curr_pos = 0;
+
+        img_file.seek(curr_pos); // use seek() bc _fseeki64 breaks on linux 
     }
 }
 
-void SrProcessor::findPattInBlock(string *block, string pattern)
+void SrProcessor::findPattInBlock(const QByteArray &block, const QByteArray &pattern)
 {
     int start_pos = 0;
-    while( 1 )
+    while(true)
     {
-        int64_t index = block->find(pattern, start_pos);
-        if( index==-1 ) // not found
+        int index = block.indexOf(pattern, start_pos);
+        if( index == -1 )
         {
             break;
         }
 
         start_pos = index + 1;
-        index += curr_pos;
+        int64_t absoluteIndex = index + curr_pos;
 
-        // only add index if it doesn't already exist
-        vector<int64_t>::iterator it;
-        it = find(positions.begin(), positions.end(),
-                  index);
-        if( it==positions.end() )
-        { // new found pattern in file
-            cout << "found " << index << " "
-                 << curr_pos << "\n";
-            positions.push_back(index);
-        }
+        if (std::find(positions.begin(), positions.end(), absoluteIndex) == positions.end())
+    {
+        cout << "found " << absoluteIndex << " " << curr_pos << "\n";
+        positions.push_back(absoluteIndex); 
+    }
     }
 }
 
 void SrProcessor::printAscii(int index, int len)
 {
-    char buffer[200];
-    int64_t cur_pos = ftell(img_file);
-    _fseeki64(img_file, positions[index], SEEK_SET);
-    int read_size = fread(buffer, 1, len, img_file);
-
-    cout << "[" << index << "/" <<  positions[index]
-         << "] Str: ";
-    for( int i=0 ; i<read_size ; i++ )
-    {
-        if( buffer[i]>31 && buffer[i]<127 )
+    int64_t original_pos = img_file.pos();
+    
+    if (img_file.seek(positions[index])) {
+        QByteArray buffer = img_file.read(len);
+        
+        cout << "[" << index << "/" <<  positions[index] << "] Str: ";
+        for( int i=0 ; i<buffer.size() ; i++ )
         {
-            printf("%c", buffer[i]);
+            unsigned char c = buffer[i];
+            if( c > 31 && c < 127 )
+                printf("%c", c);
+            else
+                printf(".");
         }
-        else
-        {
-            printf(".");
-        }
+        cout << endl;
     }
-    cout << endl;
-
-    _fseeki64(img_file, cur_pos, SEEK_SET);
+    
+    img_file.seek(original_pos);
 }
 
 void SrProcessor::printHex(int index, int len)
 {
-    char buffer[200];
-    int64_t cur_pos = ftell(img_file);
-    _fseeki64(img_file, positions[index], SEEK_SET);
-    int read_size = fread(buffer, 1, len, img_file);
+    int64_t original_pos = img_file.pos();
 
-    cout << "[" << index << "] Hex: ";
-    if( read_size>20 )
-    {
-        read_size = 20;
-    }
-    for( int i=0 ; i<read_size ; i++ )
-    {
-        printf("0x%x ", buffer[i]);
-    }
-    cout << endl;
+    if (img_file.seek(positions[index])) {
+        QByteArray buffer = img_file.read(len);
 
-    _fseeki64(img_file, cur_pos, SEEK_SET);
+        cout << "[" << index << "] Hex: ";
+        int printLen = std::min((int)buffer.size(), 20);
+        
+        for( int i=0 ; i<printLen ; i++ )
+        {
+            printf("0x%x ", (unsigned char)buffer[i]);
+        }
+        cout << endl;
+    }
+
+    img_file.seek(original_pos);
 }
 
 // read from file and write to the output till the input position
 void SrProcessor::rwUntilPosition(int64_t position)
 {
-    while( curr_pos<position )
+    while( curr_pos < position )
     {
-        int read_size = SR_BLOCK_SIZE;
-        if( curr_pos+SR_BLOCK_SIZE>position )
+        int64_t to_read = SR_BLOCK_SIZE;
+        if( curr_pos + SR_BLOCK_SIZE > position )
         {
-            read_size = position - curr_pos;
+            to_read = position - curr_pos;
         }
 
-        fread(f_buffer, 1, read_size, img_file);
-        fwrite(f_buffer, 1, read_size, raw_file);
-        curr_pos += read_size;
+        QByteArray buffer = img_file.read(to_read);
+        if (buffer.isEmpty()) break;
+        
+        raw_file.write(buffer);
+        curr_pos += buffer.size();
     }
 }
