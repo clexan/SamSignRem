216baf6f0227a05f094de7ca47779e56b72cf81d
Martin <clexan.priv@protonmail.com>
2026-01-04 19:02:25 +0100

    replace system() with qprocess and use qdir

--- Sources/sr_lz4.cpp
+++ Sources/sr_lz4.cpp
@@ -1,17 +1,31 @@
-#include "sr_lz4.h"
-
 void sr_lz4Decode()
 {
     sr_mkdir("lz4");
 
     vector<string> lz4s = sr_findlz4s();
     int lz4_len = lz4s.size();
+    
+    cout << "Decoding " << lz4_len << " lz4 files..." << endl;
+
     for( int i=0 ; i<lz4_len ; i++ )
     {
-        string name_without_lz4 = lz4s[i].substr(0, lz4s[i].size()-4);
-        string cmd = "lz4.exe " + lz4s[i] + " lz4\\" +
-                name_without_lz4 + "\n";
-        system(cmd.c_str());
+        QString inputFile = QString::fromStdString(lz4s[i]);
+        // Remove .lz4 extension for the output filename
+        QString outputFile = "lz4/" + inputFile.left(inputFile.length() - 4);
+        
+        // Use QProcess instead of system()
+        QString program = "lz4";
+        QStringList arguments;
+        // -d: decompression, -f: overwrite output without prompting
+        arguments << "-d" << "-f" << inputFile << outputFile;
+
+        QProcess process;
+        process.start(program, arguments);
+        process.waitForFinished();
+        
+        if (process.exitCode() != 0) {
+            cout << "Failed to decode: " << inputFile.toStdString() << endl;
+        }
     }
 }
 
@@ -21,32 +35,44 @@ void sr_lz4Code()
 
     vector<string> imgs = sr_findImgs("patched");
     int imgs_len = imgs.size();
+
+    cout << "Encoding " << imgs_len << " patched files..." << endl;
+
     for( int i=0 ; i<imgs_len ; i++ )
     {
-        string cmd = "lz4.exe patched\\" + imgs[i] + " tar\\" +
-                imgs[i] + ".lz4\n";
-        system(cmd.c_str());
+        QString inputFile = "patched/" + QString::fromStdString(imgs[i]);
+        QString outputFile = "tar/" + QString::fromStdString(imgs[i]) + ".lz4";
+
+        QString program = "lz4";
+        QStringList arguments;
+        // -f: overwrite output
+        arguments << "-f" << inputFile << outputFile;
+
+        QProcess process;
+        process.start(program, arguments);
+        process.waitForFinished();
+        
+        if (process.exitCode() != 0) {
+             cout << "Failed to encode: " << inputFile.toStdString() << endl;
+        }
     }
 }
 
 vector<string> sr_findlz4s()
 {
     vector<string> ret;
-    string path = sr_getCurrentPath();
-    DIR    *dir = opendir(path.c_str());
+    QDir dir = QDir::current();
 
-    dirent *file = readdir(dir);
-    while( file )
-    {
-        string filename = file->d_name;
-        int have_lz4 = filename.find(".lz4");
-        if( have_lz4!=-1 )
-        {
-            ret.push_back(filename);
-        }
-        file = readdir(dir);
+    // Set filters for .lz4 files
+    QStringList filters;
+    filters << "*.lz4";
+    dir.setNameFilters(filters);
+    dir.setFilter(QDir::Files | QDir::NoDotAndDotDot);
+
+    QFileInfoList list = dir.entryInfoList();
+    for (const QFileInfo &fileInfo : list) {
+        ret.push_back(fileInfo.fileName().toStdString());
     }
-    closedir(dir);
 
     return ret;
 }
